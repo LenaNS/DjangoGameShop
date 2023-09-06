57ddf5d5616682d3eef3ba27959abf5b54b5ab99
LenaNS <lena.noskova.99@mail.ru>
2023-09-06 19:07:56 +0800

    Изучение Django

---
+++ games/views.py
@@ -0,0 +1,19 @@
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from games.models import Game
+
+
+class GameListView(ListView):
+    model = Game
+    template_name = 'games/home.html'
+    context_object_name = 'game_list'
+
+
+class GameDetailView(LoginRequiredMixin, DetailView):
+    model = Game
+    context_object_name = 'game'
+
+
+
